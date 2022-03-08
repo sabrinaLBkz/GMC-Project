782bc1046cb2806b5fe2b78fe2813e343cb9d8b0
sabrinaLBkz <93838974+sabrinaLBkz@users.noreply.github.com>
2022-03-08 14:50:41 +0100

    Add files via upload

---
+++ Alumni/js/alumni.js
@@ -0,0 +1,46 @@
+/*home-JS*/
+const nav = document.querySelector('.nav')
+window.addEventListener('scroll', fixNav)
+
+function fixNav() {
+    if (window.scrollY > nav.offsetHeight + 150) {
+        nav.classList.add('active')
+    } else {
+        nav.classList.remove('active')
+    }
+}
+
+
+/*event-JS*/
+const eventContainer = document.querySelector(".event-container")
+const slideRight = document.querySelector(".right-slide")
+const slideLeft = document.querySelector(".left-slide")
+const upButton = document.querySelector(".up-button")
+const downButton = document.querySelector(".down-button")
+const slidesLength = slideRight.querySelectorAll("div").length;
+
+let activeSlideIndex = 0
+
+slideLeft.style.top = `-${(slidesLength - 1) * 100}vh`
+
+upButton.addEventListener('click', () => changeSlide('up'))
+downButton.addEventListener('click', () => changeSlide('down'))
+
+const changeSlide = (direction) => {
+    const sliderHeight = eventContainer.clientHeight
+    if (direction === 'up') {
+        activeSlideIndex++
+        if (activeSlideIndex > slidesLength - 1) {
+            activeSlideIndex = 0
+        }
+    } else if (direction === 'down') {
+        activeSlideIndex--
+        if (activeSlideIndex < 0) {
+            activeSlideIndex = slidesLength - 1
+        }
+    }
+
+    slideRight.style.transform = `translateY(-${activeSlideIndex * sliderHeight}px)`
+    slideLeft.style.transform = `translateY(${activeSlideIndex * sliderHeight}px)`
+
+}
